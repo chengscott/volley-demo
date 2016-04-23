f285c15bf69e28cd66e9d731d309f7b1e6a77ff7
dev_Scott <chengscott@live.com>
2016-04-23 17:08:18 +0800

    json request

--- app/src/main/java/io/github/chengscott/volleydemo/MainActivity.java
+++ app/src/main/java/io/github/chengscott/volleydemo/MainActivity.java
@@ -2,12 +2,105 @@ package io.github.chengscott.volleydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    final String pokemonTag = "Pokemon";
+    final String htcgTag = "HTCG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPokemonObject();
+    }
+
+    private void getPokemonObject() {
+        String url = "http://chengscott.tw/talk/page/Google_Volley_Intro/json/pokemon.json";
+        JsonObjectRequest listItemRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        List<String> pokemons = new ArrayList<>();
+                        for (int i = 1; i < response.length(); ++i) {
+                            JSONObject pokemon;
+                            try {
+                                pokemon = response.getJSONObject(Integer.toString(i));
+                                pokemons.add(pokemon.getString("name"));
+                            } catch (Exception e) {
+
+                            } finally {
+
+                            }
+                        }
+                        PopulateListView(pokemons);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        AppController.getInstance().addToRequestQueue(listItemRequest, pokemonTag);
+    }
+
+    private void getHTCGArray() {
+        String url = "http://chengscott.tw/talk/page/Google_Volley_Intro/json/htcg.json";
+        JsonArrayRequest listItemRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        List<String> talks = new ArrayList<>();
+                        for (int i = 0; i < response.length(); ++i) {
+                            JSONObject talk;
+                            try {
+                                talk = (JSONObject) response.get(i);
+                                talks.add(talk.getString("title"));
+                            } catch (Exception e) {
+
+                            } finally {
+
+                            }
+                        }
+                        PopulateListView(talks);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        AppController.getInstance().addToRequestQueue(listItemRequest, htcgTag);
+    }
+
+    private void PopulateListView(List list) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list);
+        ((ListView) findViewById(R.id.listView)).setAdapter(adapter);
     }
 }
